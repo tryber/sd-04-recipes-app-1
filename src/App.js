3f1fd4267ee748f3ef8fc6919f6488a9e4938196
binhus <eng.robsonwilli@gmail.com>
2020-07-30 20:25:19 -0300

    Segunda vez tentando vencer o CC

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Login from './Pages/Login';
 import Comidas from './Pages/Comidas';
+import Login from './Pages/Login';
 
 function App() {
   return (
