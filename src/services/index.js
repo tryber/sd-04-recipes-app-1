feb38df78db54a044db66ed427a55d18d0486d64
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-07-31 15:28:16 -0300

    links

---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+const categories = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+const areas = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
+const ingredientes = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
